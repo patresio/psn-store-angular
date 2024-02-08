b1cf8101a079efeae2aee21acd8b89335c300e5d
Ricardo Pereira <patresio@gmail.com>
2024-02-08 16:06:04 -0300

    first commit

---
+++ src/assets/fakeDB.ts
@@ -0,0 +1,32 @@
+export const fakeDB = [
+    {
+        cover: "assets/game-covers/ac-cover.jpg",
+        gameName: "Assassin´s Creed",
+        price: "RS 79,99"
+    },
+    {
+        cover: "assets/game-covers/bt-1.jpg",
+        gameName: "Battlefield 1",
+        price: "RS 209,99"
+    },
+    {
+        cover: "assets/game-covers/bt-4.jpg",
+        gameName: "Battlefield 4",
+        price: "RS 229,99"
+    },
+    {
+        cover: "assets/game-covers/bt-5.jpg",
+        gameName: "Battlefield 5",
+        price: "RS 379,99"
+    },
+    {
+        cover: "assets/game-covers/bt-bad-company-2.jpg",
+        gameName: "Battlefield Bad Company 2",
+        price: "RS 299,99"
+    },
+    {
+        cover: "assets/game-covers/bt-hardline.jpg",
+        gameName: "Battlefield Hardline",
+        price: "RS 259,99"
+    }
+]
